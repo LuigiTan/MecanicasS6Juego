4cdd6dfc4c558df0fd5247fbded05e0e8f6cb50f
LuigiTan <gibran.garcia@uniat.edu.mx>
2025-06-30 14:48:43 -0600

    Weapons finally work and do damage. Some bugs need fixing

--- Assets/WeaponScriptsGGC/AoEWeapon.cs
+++ Assets/WeaponScriptsGGC/AoEWeapon.cs
@@ -15,13 +15,20 @@ public class AoEWeapon : BaseWeapon
             return;
         }
 
-        GameObject proj = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+        // Obtener dirección hacia donde mira la cámara
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Vector3 targetPoint = ray.GetPoint(100f); // Para ajustar la distancia del ray 
+        Vector3 direction = (targetPoint - shootPoint.position).normalized;
+
+        // Instanciar proyectil con esa dirección
+        GameObject proj = Instantiate(projectilePrefab, shootPoint.position, Quaternion.LookRotation(direction));
 
         Rigidbody rb = proj.GetComponent<Rigidbody>();
         if (rb != null)
         {
-            Vector3 force = shootPoint.forward * projectileForce + shootPoint.up * upwardForce;
-            rb.AddForce(force, ForceMode.Impulse);
+            Vector3 launchForce = direction * projectileForce + shootPoint.up * upwardForce;
+            rb.AddForce(launchForce, ForceMode.Impulse);
         }
+        Debug.DrawRay(shootPoint.position, direction * 3f, Color.yellow, 2f);
     }
 }
